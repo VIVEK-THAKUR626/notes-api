9911ec33338c00851f9e29077bb378b3331ba65e
VIVEK-THAKUR626 <vistaar4324@gmail.com>
2026-09-04 20:46:24 +0530

    create a /notes endpoint

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const app = express();
 
 app.use(express.json());
 
+let notes = [];
+
 app.get("/",(req,res) => {
     res.send("Server is working");
+})
+
+app.get("/notes", (req,res) => {
+    res.json(notes);
+})
+
+app.post("/notes", (req,res) => {
+    const note = req.body;
+
+    notes.push(note);
+
+    res.json(notes);
 })
 
 app.listen(3000,() => {
